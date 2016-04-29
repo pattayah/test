3fbf2175ffea1c510dd6630315cab1108cb05f93
pattayah <pattaya.h@gmail.com>
2016-04-29 06:32:41 -0400

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,56 +1,60 @@
 # HOW DO I LOAD THE MESSAGING PLATFORM, CLUSTER, AND LIBRARY???
+require 'nsq'
 
 # Start a queue of 100_000 nsqd's and 100 video_id's.
 # This will block execution until all components are fully up and running.
-queue = NsqCluster.new(message: 100_000, video_id: 100)
+queue1 = NsqCluster.new(message: 90_000, video_id: 50)		# videos with more than 100 plays
+queue2 = NsqCluster.new(message: 1_000, video_id:50)			# videos with fewer than 100 plays
+queue = queue1.merge(queue2)
 
 # Connect directly to a single nsqd instance
 nsqd = queue.nsqd.first			# DO I DO THIS OR NSQLOOKUP BELOW???
 
-# client initialization
-client = Nsq::Consumer.new(
+# processor initialization
+processor = Nsq::Consumer.new(
 	topic: 'topic',
 	channel: 'channel',
 	nsqlookupd: queue,			# DO I DO THIS OR NSQD ABOVE???
-	max_in_flight: 20			# max number of messages to have in flight at a time
+	discovery_interval: ,		# SHOULD THIS BE LOW SO CAN CHECK IN REAL TIME OR DEFAULT BECAUSE WE NEED TO UPDATE EVERY MINUTE
+	max_in_flight: 20			# IS THIS RIGHT???
 	)
 
-play_count = Hash.new(0)
-
-# count the plays
-queue.each do | msg, id |
-	play_count[id] += 1
-	if play_count[id] < 100 && # hasn't updated????
-		# publish to the client asap
-		# DO I USE THIS OR POP_WITHOUT_BLOCKING LIKE BELOW???
-		processor = client.pop
-	else
-		# play count needs to be up to date for the last minute
-		sleep 0.01
-	end
-end
+client = Logger.new
 
 # HOW DO I PROVIDE PLAYCOUNTS FOR UP TO 20 VIDEOS IN A SINGLE MESSAGE???
 # HOW DO I MAKE SURE THE PLAY COUNT IS ONLY PUBLISHED IF IT'S CHANGED SINCE IT WAS LAST PUBLISHED???
 
+class processor < Nsq::Consumer
 
-# If there are messages on the queue, pop will return one immediately
-# DO I USE THIS OR POP WITH BLOCKING LIKE ABOVE???
-loop do
-  if processor = @messages.pop_without_blocking
-    # do something
-    processor.finish
-  else
-    # wait for a bit before checking for new messages
-    sleep 0.01
-  end
+	# initialize play count at 0
+	@@count = {}
+
+	def initialize(msg, video_id)
+		@msg = msg
+		@video_id = video_id
+		@@count[video_id] = {}
+	end
+
+	# counts the plays
+	def play_count
+		@msg = client.pop
+	end
+
+	# real-time updates for videos with under 100 plays
+	def push_under_100 (video_id)
+		if @count < 100
+
+		else
+			push_over_100
+		end
+	end
+
+	# wait to push for videos with over 100 plays
+	def push_over_100 (video_id)
+
+	end
+
 end
 
-# Publish a message to a topic
-nsqd.pub('stats', 'a message')
-
-# Get stats in JSON format
-nsqd.stats
-
-# To enable logging
+# IDK IF THIS IS RIGHT???
 Nsq.client = Logger.new(STDOUT)
