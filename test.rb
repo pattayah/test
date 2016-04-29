77ac7a3cacc54ef72ab027ffe328bf07c650857f
pattayah <pattaya.h@gmail.com>
2016-04-29 14:22:03 -0400

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,60 +1,50 @@
 # HOW DO I LOAD THE MESSAGING PLATFORM, CLUSTER, AND LIBRARY???
 require 'nsq'
+Dir["/Documents/Wistia/nsb-cluster-master/*.rb"].each {|file| require file }
+Dir["/Documents/Wistia/nsb-ruby-master/*.rb"].each {|file| require file }
 
 # Start a queue of 100_000 nsqd's and 100 video_id's.
 # This will block execution until all components are fully up and running.
-queue1 = NsqCluster.new(message: 90_000, video_id: 50)		# videos with more than 100 plays
-queue2 = NsqCluster.new(message: 1_000, video_id:50)			# videos with fewer than 100 plays
-queue = queue1.merge(queue2)
+# HOW DO I ACCOUNT FOR THE RIGHT DISTRIBUTION
+queue = NsqCluster.new(message: 100_000, video_id: 100)
 
 # Connect directly to a single nsqd instance
-nsqd = queue.nsqd.first			# DO I DO THIS OR NSQLOOKUP BELOW???
+nsqd = queue.nsqd.first                     # DO I DO THIS OR CAN I KEEP THE PROCESSOR TASKS BELOW???
 
-# processor initialization
-processor = Nsq::Consumer.new(
-	topic: 'topic',
-	channel: 'channel',
-	nsqlookupd: queue,			# DO I DO THIS OR NSQD ABOVE???
-	discovery_interval: ,		# SHOULD THIS BE LOW SO CAN CHECK IN REAL TIME OR DEFAULT BECAUSE WE NEED TO UPDATE EVERY MINUTE
-	max_in_flight: 20			# IS THIS RIGHT???
-	)
-
-client = Logger.new
-
-# HOW DO I PROVIDE PLAYCOUNTS FOR UP TO 20 VIDEOS IN A SINGLE MESSAGE???
 # HOW DO I MAKE SURE THE PLAY COUNT IS ONLY PUBLISHED IF IT'S CHANGED SINCE IT WAS LAST PUBLISHED???
 
-class processor < Nsq::Consumer
+class Processor
 
-	# initialize play count at 0
-	@@count = {}
+	def initialize
+		@count = {}
+        @over_100 = {}
+    end
 
-	def initialize(msg, video_id)
-		@msg = msg
-		@video_id = video_id
-		@@count[video_id] = {}
-	end
-
-	# counts the plays
+	# count the plays
 	def play_count
-		@msg = client.pop
+		parsed_msg = JSON.parse(next_message)
+        video_id = parsed_msg["video_id"]
+        @count[video_id] += 1
 	end
 
-	# real-time updates for videos with under 100 plays
-	def push_under_100 (video_id)
-		if @count < 100
-
-		else
-			push_over_100
+	# publish to the client
+	def publish(video_id)
+        if @count[video_id] < 100           # videos with under 100 plays
+            # publish to client immediately
+            Nsq.client = Logger.new(STDOUT)
+        elsif @over_100.length < 20         # storing up to 20 videos with over 100 plays
+            @over_100[video_id] = @count[video_id]
+        else                                # publish to the client
+            Nsq.client = Logger.new(STDOUT)
 		end
 	end
-
-	# wait to push for videos with over 100 plays
-	def push_over_100 (video_id)
-
-	end
-
 end
 
-# IDK IF THIS IS RIGHT???
-Nsq.client = Logger.new(STDOUT)
+# create a new Processor
+processor = Processor.new
+
+# read messages off the queue
+queue.each do |msg|
+    processor.update(msg)
+    processor.publish(queue[msg])
+end
